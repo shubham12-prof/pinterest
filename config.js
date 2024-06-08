fca16eac721463331a40a124ec758a828c2ea6e4
shubham12-prof <shubhsemwal12@gmail.com>
2024-06-09 02:22:43 +0530

    vercel db changes

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 
 async function mongooseConnect() {
     await mongoose.connect(`mongodb+srv://shubhsemwal12:${encodeURIComponent('Semwal@12')}@cluster0.cbza67r.mongodb.net/pintmernclone?retryWrites=true&w=majority&appName=Cluster0`, {
